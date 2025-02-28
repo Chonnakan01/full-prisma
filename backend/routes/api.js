cf128b51bf49a43b8f4af6430a4a8a745cb5da0c
Chonnakan01 <chanakarn.srih@ku.th>
2025-02-28 16:06:12 +0700

    new

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -16,4 +16,12 @@ router.delete('/customers/:id',  customerController.deleteCustomer);
 router.get('/customers/:id',  customerController.getCustomer);
 router.get('/customers',  customerController.getCustomers);
 
+const productController = require('../controllers/products');
+
+router.post('/products', apiLimiter, productController.createProduct);
+router.put('/products/:id', productController.updateProduct);
+router.delete('/products/:id', productController.deleteProduct);
+router.get('/products/:id', productController.getProduct);
+router.get('/products', productController.getProducts);
+
 module.exports = router;
